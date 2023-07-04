8cf868a5881dca0ad91c385c4ec6b2d5fdaf8c35
Devinaasalsa <devinasdiva@gmail.com>
2023-07-04 10:54:39 +0700

    testing

---
+++ pkg/models/mapping_code.go
@@ -0,0 +1,14 @@
+package models
+
+type MappingCode struct {
+	BaseModel
+	ActiveModel
+	StatusModel
+	Code       string `json:"code" form:"code" validate:"required"`
+	Definition string `json:"definition" form:"definition" gorm:"uniqueIndex" validate:"required"`
+	Status     bool `json:"status" form:"status" validate:"required"`
+	Priority   int `json:"priority" form:"priority" validate:"required"`
+	IsActive   bool `json:"is_active" form:"is_active" validate:"required"`
+
+
+}
